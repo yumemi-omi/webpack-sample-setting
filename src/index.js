396d3cfcf85e64e5022b3ee8c7163590a14a4d5c
yumemi-homi <h_omi@yumemi.co.jp>
2019-08-20 18:41:47 +0900

    はいよ

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 import "./styles.scss";
 
-class App extends React.Component {
-  render() {
-    return <div>Hello {this.props.name}</div>;
-  }
-}
+import App from "./App";
 
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App name="Jane" />, mountNode);
